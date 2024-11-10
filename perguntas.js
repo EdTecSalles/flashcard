c9cf3a7ad47faa21d1784ce26bcc9fa4b825dbc9
EdTecSalles <ednilsonsantos@prof.educacao.sp.gov.br>
2024-11-10 17:43:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação', 
+    'O que é Phyton?', 
+    'O Javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia', 
+    'Qual é a capital da França?', 
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação', 
+    'O que é uma função?', 
+    'Função é um bloco de código que executa uma tarefa'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hi (RAI).'
+)
